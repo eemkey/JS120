c56ec67e327d220b19bd52d39858388cd7ef5e93
Emily Ma <em.kayama@gmail.com>
2020-09-06 21:50:59 -0400

    implement keeping score functionality

--- lesson_1/oo_rps.js
+++ lesson_1/oo_rps.js
@@ -1,30 +1,23 @@
-/* RPS is a two player game in which each player
-chooses one of three possible moves:
-rock, paper, or scissors.
-The winner is chosen based on the following rules:
-rock crushes scissors (rock wins)
-scissors cuts paper (scissors wins)
-paper wraps rock (paper wins)
-if both players choose the same move, the game is a tie
-
-Nouns: player, move, rule
-Verbs: choose, compare
-
-Player
-  - choose
-Move
-Rule
-
-???
-  - compare
-*/
-
 const readline = require("readline-sync");
+const POINTS_TO_WIN = 3;
+const VALID_CHOICES = {
+  rock: ["rock", "r"],
+  paper: ["paper", "p"],
+  scissors: ["scissors", "sc"],
+};
 
 function createPlayer() {
   return {
     move: null
   };
+}
+
+function convertChoice(input) {
+  for (let prop in VALID_CHOICES) {
+    if (VALID_CHOICES[prop].includes(input)) {
+      return prop;
+    }
+  }
 }
 
 function createHuman() {
@@ -37,11 +30,11 @@ function createHuman() {
       while (true) {
         console.log("Please choose rock, paper, or scissors:");
         choice = readline.question();
-        if (["rock", "paper", "scissors"].includes(choice)) break;
+        if ([].concat(...Object.values(VALID_CHOICES)).includes(choice)) break;
         console.log("Sorry, invalid choice.");
       }
 
-      this.move = choice;
+      this.move = convertChoice(choice);
     }
   };
   return Object.assign(playerObj, humanObj);
@@ -52,12 +45,20 @@ function createComputer() {
 
   let computerObj = {
     choose() {
-      const choices = ["rock", "paper", "scissors"];
+      const choices = Object.keys(VALID_CHOICES);
       let randomIdx = Math.floor(Math.random() * choices.length);
       this.move = choices[randomIdx];
     }
   };
   return Object.assign(playerObj, computerObj);
+}
+
+function createScoreBoard() {
+  return {
+    pointsToWin: POINTS_TO_WIN,
+    humanScore: 0,
+    computerScore: 0
+  }
 }
 
 // function createMove() {
@@ -74,9 +75,19 @@ function createComputer() {
 //   // not yet implemented
 // };
 
+
+
 const RPSGame = {
+
   human: createHuman(),
   computer: createComputer(),
+  scoreBoard: createScoreBoard(),
+
+  WINNING_COMBOS: {
+    rock: ["scissors"],
+    paper: ["rock"],
+    scissors: ["paper"],
+  },
 
   displayWelcomeMessage() {
     console.log("Welcome to Rock, Paper, Scissors!");
@@ -84,38 +95,80 @@ const RPSGame = {
   displayGoodbyeMessage() {
     console.log("Thanks for playing, Rock, Paper, Scissors. Goodbye!");
   },
-  displayWinner() {
-    let humanMove = this.human.move;
-    let computerMove = this.computer.move;
+  displayWinner(outcome, humanMove, computerMove) {
     console.log(`You chose ${humanMove}`);
     console.log(`The computer chose ${computerMove}`);
 
+    if (outcome === "human") {
+      console.log("You win this round!");
+    } else if (outcome === "computer") {
+      console.log("Computer wins this round!");
+    } else {
+      console.log("It's a tie this round!");
+    }
+  },
+  displayCurrentScore(scoreBoard) {
+    console.log(`human: ${scoreBoard.humanScore} computer: ${scoreBoard.computerScore}`);
+  },
+  updateScoreBoard(outcome) {
+    if (outcome === "human") {
+      this.scoreBoard.humanScore++;
+    } else if (outcome === "computer"){
+      this.scoreBoard.computerScore++;
+    }
+  },
+  determineOutcome(humanMove, computerMove) {
     if ((humanMove === "rock" && computerMove === "scissors") ||
         (humanMove === "paper" && computerMove === "rock") ||
         (humanMove === "scissors" && computerMove === "paper")) {
-      console.log("You win!");
+      return "human";
     } else if ((humanMove === "rock" && computerMove === "paper") ||
                (humanMove === "paper" && computerMove === "scissors") ||
                (humanMove === "scissors" && computerMove === "rock")) {
-      console.log("Computer wins!");
+      return "computer";
     } else {
-      console.log("It's a tie");
+      return "tie";
     }
   },
+
+  displayGrandWinner(scoreBoard) {
+    if (scoreBoard.humanScore === POINTS_TO_WIN) {
+      console.log("You win!");
+    } else {
+      console.log("Computer wins!");
+    }
+  },
+
+  detectGrandWinner(scoreBoard) {
+    return scoreBoard.computerScore === POINTS_TO_WIN || scoreBoard.humanScore === POINTS_TO_WIN;
+  },
+
 
   playAgain() {
     console.log("Would you like to play again? (y/n)");
     let answer = readline.question();
     return answer.toLowerCase()[0] === "y";
   },
+
   play() {
     this.displayWelcomeMessage();
+
     while (true) {
-      this.human.choose();
-      this.computer.choose();
-      this.displayWinner();
+      this.scoreBoard = createScoreBoard();
+      while (!this.detectGrandWinner(this.scoreBoard)) {
+        this.human.choose();
+        this.computer.choose();
+  
+        let outcome = this.determineOutcome(this.human.move, this.computer.move);
+        this.updateScoreBoard(outcome);
+        this.displayWinner(outcome, this.human.move, this.computer.move);
+        this.displayCurrentScore(this.scoreBoard);
+      }
+
+      this.displayGrandWinner(this.scoreBoard);
       if (!this.playAgain()) break;
     }
+
     this.displayGoodbyeMessage();
   }
 };
